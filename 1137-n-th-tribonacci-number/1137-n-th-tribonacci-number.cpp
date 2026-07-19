f14dea37c6f323242c69e0e7c895ca8180367340
kanhaiya-09 <kanhaiya09042005@gmail.com>
2026-07-19 11:22:51 +0530

    Time: 0 ms (100%), Space: 8.5 MB (25.61%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -4,11 +4,11 @@ public:
     int tribo(int n, vector<int>& dp){
         if(n==2) return 1;
         if(n<2) return n;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = tribo(n-1, dp)+tribo(n-2, dp)+tribo(n-3, dp);
+        if(dp[n-1]!=-1) return dp[n-1];
+        return dp[n-1] = tribo(n-1, dp)+tribo(n-2, dp)+tribo(n-3, dp);
     }
     int tribonacci(int n) {
-        dp.resize(n+1,-1);
+        dp.resize(n,-1);
         return tribo(n, dp);
     }
 };
