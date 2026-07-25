cf953fb3894ec27394c4b7a7fa3c6fd9692c0f4f
kanhaiya-09 <kanhaiya09042005@gmail.com>
2026-07-25 23:44:19 +0530

    Time: 0 ms (100%), Space: 15.4 MB (91.33%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-
-        int m = grid.size();
-        int n = grid[0].size();
-
-        if (i >= m || j >= n)
-            return 1e9;   // Large value instead of INT_MAX
-
-        if (i == m - 1 && j == n - 1)
-            return grid[i][j];
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int right = solve(i, j + 1, grid, dp);
-        int down = solve(i + 1, j, grid, dp);
-
-        return dp[i][j] = grid[i][j] + min(right, down);
-    }
-
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-
-        return solve(0, 0, grid, dp);
+        int ans[m][n];
+        int i, j = 1;
+        ans[0][0] = grid[0][0];
+        for(int k = 1;k < n;k++){
+            ans[0][k] = grid[0][k] + ans[0][k-1];
+        }
+        for(int k = 1;k < m;k++){
+            ans[k][0] = grid[k][0] + ans[k-1][0];
+        }
+        for(int k =1;k<m;k++){
+            for(int p=1; p<n;p++){
+                ans[k][p] = min(ans[k-1][p], ans[k][p-1]) + grid[k][p];
+            }
+        }
+        return ans[m-1][n-1];
     }
 };
